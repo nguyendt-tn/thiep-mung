9dcbb6c97b3a6966475777b8fb123863f0e44e1f
nguyendt-tn <68462211+nguyendt-tn@users.noreply.github.com>
2021-03-08 21:55:53 +0700

    Update present.js

--- present.js
+++ present.js
@@ -1,6 +1,6 @@
 
 var to = 'Quyên!';
-var gift_url = 'https://scontent-sin6-1.xx.fbcdn.net/v/t1.0-0/c0.0.600.600a/p600x600/156707489_1649015805284781_5762603683497178858_o.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=dqz8WuT4axQAX9vVD4n&_nc_ht=scontent-sin6-1.xx&tp=27&oh=7a981399544e2d26d37cb295321369bf&oe=606B5CFF';
+var gift_url = '#';
 var gift_image_url = 'https://scontent-sin6-1.xx.fbcdn.net/v/t1.0-0/c0.0.600.600a/p600x600/156707489_1649015805284781_5762603683497178858_o.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=dqz8WuT4axQAX9vVD4n&_nc_ht=scontent-sin6-1.xx&tp=27&oh=7a981399544e2d26d37cb295321369bf&oe=606B5CFF';
 
 
